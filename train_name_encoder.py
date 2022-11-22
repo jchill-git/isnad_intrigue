d24e53f16db8f27d30518364fec18a0b13cd3c5a
Kyle Sayers <kylesayrs@gmail.com>
2022-11-21 21:59:12 -0500

    baka

--- train_name_encoder.py
+++ train_name_encoder.py
@@ -3,11 +3,12 @@ import wandb
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizerFast, BertModel
+from transformers import logging
 
 from helpers.data import read_isnad_names, read_isnad_data
 
-
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+logging.set_verbosity_error()
 
 class NamesDataset(Dataset):
     def __init__(self, isnad_names_path: str, isnad_labels_path: str):
@@ -38,7 +39,11 @@ if __name__ == "__main__":
         entity="kylesayrs",
         config={
             "num_epochs": 3,
-            "batch_size": 32
+            "batch_size": 32,
+            "max_length": 32,
+            "learning_rate": 0.01,
+            "momentum": 0.9,
+            "temperature": 1.0
         },
         mode="disabled"
     )
@@ -54,6 +59,10 @@ if __name__ == "__main__":
         batch_size=1,
         shuffle=True,
         num_workers=0,
+        collate_fn=lambda data: [
+            data[0][0],
+            data[0][1],
+        ]
     )
 
     key_dataloader = DataLoader(
@@ -61,45 +70,102 @@ if __name__ == "__main__":
         batch_size=wandb.config["batch_size"],
         shuffle=True,
         num_workers=0,
+        collate_fn=lambda data: [
+            [name for name, _ in data],
+            [label for _, label in data],
+        ]
     )
 
-    # set up model and loss
+    # set up model, optimizer, and loss
     tokenizer = BertTokenizerFast.from_pretrained(
         "lanwuwei/GigaBERT-v3-Arabic-and-English",
         do_lower_case=True,
         to=DEVICE,
     )
     model = BertModel.from_pretrained("lanwuwei/GigaBERT-v3-Arabic-and-English").to(DEVICE)
-    criterion = torch.nn.TripletMarginLoss(margin=1)
+    criterion = torch.nn.CosineEmbeddingLoss()#torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=wandb.config["learning_rate"],
+        momentum=wandb.config["momentum"]
+    )
 
     for epoch in range(wandb.config["num_epochs"]):
         for (query_name, query_label), (key_names, key_labels) in zip(query_dataloader, key_dataloader):
 
-            # collate
-            query_name = query_name[0]
-            key_names = list(key_names)
+            # reset gradient
+            optimizer.zero_grad()
 
-            print(query_name)
-            print(query_label)
-            print(key_names)
-            print(key_labels)
+            # embed query
+            query_tokens = tokenizer(
+                query_name,
+                max_length=wandb.config["max_length"],
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt"
+            )
+            query_output = model(**query_tokens).last_hidden_state
+            query_output = query_output[:, 0, :] # take cls token
+            #query_output = torch.nn.functional.normalize(query_output, dim=1)
 
-            query_tokens = tokenizer(query_name, return_tensors="pt")
-            print(query_tokens)
-            query_output = model(**query_tokens)[0]
-            query_output = query_output[0] # take cls token
-            print(query_output)
+            # embed keys
+            key_tokens = tokenizer(
+                key_names,
+                is_split_into_words=False,
+                max_length=wandb.config["max_length"],
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt"
+            )
+            key_outputs = model(**key_tokens).last_hidden_state
+            key_outputs = key_outputs[:, 0, :] # take cls token
+            #key_outputs = torch.nn.functional.normalize(key_outputs, dim=1)
 
-            key_tokens = tokenizer(key_names, return_tensors="pt")
-            print(key_tokens)
-            key_outputs = model(**key_tokens)[0]
-            key_outputs = key_outputs[0] # take cls token
-            print(key_outputs)
+            # separate into positive and negative outputs
+            #positive_labels_mask = [key_label == query_label for key_label in key_labels]
+            #negative_labels_mask = [key_label != query_label for key_label in key_labels]
+            #positive_outputs = key_outputs[positive_labels_mask]
+            #negative_outputs = key_outputs[negative_labels_mask]
 
-            positive_outputs = key_outputs[query_label == key_labels]
-            negative_outputs = key_outputs[query_label != key_labels]
+            """
+            # compute logits
+            positive_logits = torch.einsum("nc,nc->n", [query_output, positive_outputs])#.unsqueeze(-1)
+            negative_logits = torch.einsum("nc,nc->n", [query_output, negative_outputs])#.unsqueeze(-1)
+            print(positive_logits)
+            print(negative_logits)
+            print(positive_logits.shape)
+            print(negative_logits.shape)
 
-            loss = criterion(query_output, positive_outputs, negative_outputs)
+            labels = torch.cat([torch.ones(len(positive_logits)), torch.zeros(len(negative_logits))])
+            logits = torch.cat([positive_logits, negative_logits], dim=0)
+            logits /= wandb.config["temperature"]
+            print(logits)
+            print(logits.shape)
+            print(labels)
+            print(labels.shape)
 
-            break
-        break
+            # compute loss
+            loss = criterion(labels, logits)
+            loss.backward()
+            optimizer.step()
+            """
+
+            print(query_output.shape)
+            #print(positive_outputs.shape)
+            #print(negative_outputs.shape)
+            print(key_outputs.shape)
+            target = torch.tensor([
+                1 if key_label == query_label else -1
+                for key_label in key_labels
+            ])
+            print(target)
+            loss = criterion(query_output, key_outputs, target)
+            print(loss)
+
+            loss.backward()
+            optimizer.step()
+
+            wandb.log({
+                "loss": loss.item()
+            })
+            print(f"loss: {loss.item()}")
