f5127077793dc24be6b2ba0470c516b5d1474b24
jchill-git <66270164+jchill-git@users.noreply.github.com>
2022-12-04 19:01:14 -0500

    Added conLL scoring

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 import json
 
-from helpers.data import read_isnad_data, split_data
+from helpers.data import read_isnad_data, split_data, _create_communities_file,createClusters,calc_conLL
 from helpers.graph import create_cooccurence_graph
 from helpers.utils import show_graph
 
@@ -37,10 +37,22 @@ if __name__ == "__main__":
         self_edges=False,
         max_isnads=None,
     )
-
+    
     #show_graph(test_graph, test_disambiguated_ids)
 
-    # evaluation
+    #create community_file
+    community_file_name='test_communities.json'
+    _create_communities_file(community_file_name,test_mention_ids)
+
+    #create model clusters
+    modelClusters = createClusters(community_file_name)
+
+    #create goldStandard clusters
+    gold_path='.\communities\goldStandard_goldTags.json'
+    goldClusters = createClusters(gold_path)
+
+    #calculate ConLL score
+    calc_conLL(goldClusters,modelClusters)
 
     # pred_graph = merge_and_stuff(test_graph)
     # pred_node_ids = pred_graph.nodes()
